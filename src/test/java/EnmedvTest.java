9a5efcd849817ccc3647ec83972892f2309df060
Ekaterina <enmedv@gmail.com>
2022-11-09 14:53:00 +0200

    EnmedvTest changed

--- src/test/java/EnmedvTest.java
+++ src/test/java/EnmedvTest.java
@@ -13,65 +13,65 @@ public class EnmedvTest {
     // 3.  Подтвердить, что вы перешли на страницу со ссылкой https://openweathermap.org/guide
     // и что title этой страницы OpenWeatherMap API guide - OpenWeatherMap
 
-//    @Test
-//    public void testH1_WhenClickBottomGuide() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
-//        WebDriver driverTask1 = new ChromeDriver();
-//
-//         // Arrange
-//        String url1 = "https://openweathermap.org/";
-//        String expectedResult1 = "Guide";
-//
-//        // Act
-//     driverTask1.get(url1);
-//     Thread.sleep(5000);
-//     WebElement searchButtonGuide = driverTask1.findElement(
-//             By.xpath("//div[@id = 'desktop-menu']//li//a[@href = '/guide']")
-//     );
-//
-//     searchBottomGuide.click();
-//
-//     WebElement h1GuideTitle = driverTask1.findElement(
-//             By.xpath("//main[@class = 'wrapper']//h1[@class = 'breadcrumb-title']")
-//     );
-//
-//     Thread.sleep(2000);
-//     String actualResult1 = h1GuideTitle.getText();
-//
-//     Assert.assertEquals(actualResult1, expectedResult1);
-//
-//     driverTask1.quit();
-//}
+    @Test
+    public void testH1_WhenClickBottomGuide() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
+        WebDriver driverTask1 = new ChromeDriver();
+
+         // Arrange
+        String url1 = "https://openweathermap.org/";
+        String expectedResult1 = "Guide";
+
+        // Act
+     driverTask1.get(url1);
+     Thread.sleep(5000);
+     WebElement searchButtonGuide = driverTask1.findElement(
+             By.xpath("//div[@id = 'desktop-menu']//li//a[@href = '/guide']")
+     );
+
+        searchButtonGuide.click();
+
+     WebElement h1GuideTitle = driverTask1.findElement(
+             By.xpath("//main[@class = 'wrapper']//h1[@class = 'breadcrumb-title']")
+     );
+
+     Thread.sleep(2000);
+     String actualResult1 = h1GuideTitle.getText();
+
+     Assert.assertEquals(actualResult1, expectedResult1);
+
+     driverTask1.quit();
+}
 
     // Задание 2
     // 1.  Открыть базовую ссылку
     // 2.  Нажать на единицы измерения Imperial: °F, mph
     // 3.  Подтвердить, что температура для города показана в Фарингейтах
 
-//    @Test
-//    public void testDivTag_WhenChangeTemperatureUnits() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
-//        WebDriver driver = new ChromeDriver();
-//        //Arrange
-//        String url = "https://openweathermap.org/";
-//        String expectedResult = "Imperial: °F, mph";
-//
-//        // Act
-//        driver.get(url);
-//        Thread.sleep(5000);
-//        WebElement searchButtonTemperatureUnits = driver.findElement(
-//                By.xpath("//div[@id = 'weather-widget']//div[text() = 'Imperial: °F, mph']")
-//        );
-//
-//        searchBottomTemperatureUnits.click();
-//
-//        Thread.sleep(2000);
-//        String actualResult = searchBottomTemperatureUnits.getText();
-//
-//        Assert.assertEquals(actualResult, expectedResult);
-//
-//        driver.quit();
-//    }
+    @Test
+    public void testDivTag_WhenChangeTemperatureUnits() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        //Arrange
+        String url = "https://openweathermap.org/";
+        String expectedResult = "Imperial: °F, mph";
+
+        // Act
+        driver.get(url);
+        Thread.sleep(5000);
+        WebElement searchButtonTemperatureUnits = driver.findElement(
+                By.xpath("//div[@id = 'weather-widget']//div[text() = 'Imperial: °F, mph']")
+        );
+
+        searchButtonTemperatureUnits.click();
+
+        Thread.sleep(2000);
+        String actualResult = searchButtonTemperatureUnits.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
 
     // Задание 3
     // 1. Открыть базовую ссылку
@@ -129,33 +129,33 @@ public class EnmedvTest {
     // 2.  Подтвердить, что в меню Support есть 3 подменю с названиями “FAQ”,
     //     “How to start” и “Ask a question”
 
-    @Test
-    public void test_WhenConfirmInMenuSupportThreeButton() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
-        WebDriver driver = new ChromeDriver();
-
-        String url = "https://openweathermap.org/";
-        String expectedResult1 = "FAQ";
-        String expectedResult2 = "How to start";
-        String expectedResult3 = "Ask a question";
-
-        driver.get(url);
-        Thread.sleep(5000);
-
-        WebElement searchMenuSupport = driver.findElement(
-                By.xpath("//div[@id = 'support-dropdown']")
-        );
-        searchMenuSupport.click();
-
-        WebElement searchButtonFAQ = driver.findElement(
-                By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[@href = '/faq']")
-        );
-
-        WebElement searchButtonHowToStart = driver.findElement(
-                By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[text() = 'How to start']")
-        );
-
-        driver.quit();
-    }
+//    @Test
+//    public void test_WhenConfirmInMenuSupportThreeButton() throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//
+//        String url = "https://openweathermap.org/";
+//        String expectedResult1 = "FAQ";
+//        String expectedResult2 = "How to start";
+//        String expectedResult3 = "Ask a question";
+//
+//        driver.get(url);
+//        Thread.sleep(5000);
+//
+//        WebElement searchMenuSupport = driver.findElement(
+//                By.xpath("//div[@id = 'support-dropdown']")
+//        );
+//        searchMenuSupport.click();
+//
+//        WebElement searchButtonFAQ = driver.findElement(
+//                By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[@href = '/faq']")
+//        );
+//
+//        WebElement searchButtonHowToStart = driver.findElement(
+//                By.xpath("//ul[@class = 'dropdown-menu dropdown-visible']//a[text() = 'How to start']")
+//        );
+//
+//        driver.quit();
+//    }
 
 }
